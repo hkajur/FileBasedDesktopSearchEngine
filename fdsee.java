4d85adbbe590542a75c6749614b56f8801eb570f
Harish Kajur <hkajur93@gmail.com>
2014-11-21 23:04:52 -0500

    Made necessary changes to make program working properly

--- fdsee.java
+++ fdsee.java
@@ -13,10 +13,94 @@ import java.nio.file.*;
 public class fdsee {
 
     public static final String SEARCH_OUTPUT = "fdsee_doclist.txt";
+    public static final String TOKEN_OUTPUT = "fdsee_token.txt";
+    public static final String TOKENDEBUG_OUTPUT = "fdsee_tokendebug.txt";
+    public static final String STOPWORD_OUTPUT = "fdsee_stopword.txt";
+    public static final String LEXICON_OUTPUT = "fdsee_lexicon.txt";
+
+    public static final String[] stopWords = {"i", "a", "about", "an", "are", "as", "at", "be", "by", "com", "en", "for", "from", "how", "in", "is", "it", "of", "on", "or", "that", "this", "to", "was", "what", "when", "where", "who", "will", "with", "www"};
 
     public static PrintWriter writer;
 
     public static int docID = 1;
+
+    public static Hashtable<String, Integer> hashWordList;
+
+    public static boolean stringInArray(String word, String[] array){
+
+        int length = array.length;
+
+        for(int i = 0; i < length; i++)
+            if(array[i].equals(word))
+                return true;
+    
+        return false;
+    }
+
+    public static void mergeAndStopWords(File file1, File file2) throws Exception {
+
+        BufferedReader br1 = new BufferedReader(new FileReader(file1));
+        BufferedReader br2 = new BufferedReader(new FileReader(file2));
+
+        PrintWriter pw = new PrintWriter(STOPWORD_OUTPUT, "UTF-8");
+
+        String line1;
+        String line2;
+
+        while(((line1 = br1.readLine()) != null)
+           && ((line2 = br2.readLine()) != null)){
+
+            line1 = line1.substring(1, line1.length() - 1);
+            line2 = line2.substring(1, line2.length() - 1);
+
+            String[] array1 = line1.split(",");
+            String[] array2 = line2.split(",");
+
+            int wordID = Integer.parseInt(array1[1]);
+            String word = array2[1];
+
+            if(stringInArray(word, stopWords)){
+                pw.println(wordID + "\t" + word + "\t" + "STOPWORD");
+            } else{
+                pw.println(wordID + "\t" + word + "\t" + "NONSTOPWORD");
+            }
+
+        }
+    
+        br1.close();
+        br2.close();
+
+        pw.close();
+    }
+   
+    public static void mergeStem(File file1) throws Exception {
+    
+        BufferedReader br = new BufferedReader(new FileReader(file1));
+
+        String line = null;
+
+        hashWordList = new Hashtable<String, Integer>();
+
+        while( ((line = br.readLine()) != null)){
+
+            String[] array1 = line.split("\t");
+                
+            int wordID = Integer.parseInt(array1[0]);
+            String word = array1[1];
+            String st = array1[2];
+            String stemmedWord = stemming.stem(word);
+
+            if(hashWordList.containsKey(stemmedWord)){
+                System.out.println(wordID + "\t" + word + "\t" + st + "\t" + hashWordList.get(stemmedWord));
+            } else {
+                hashWordList.put(word, wordID);
+                System.out.println(wordID + "\t" + word + "\t" + st + "\t" + wordID);
+            }
+        
+        }
+
+        br.close();
+    }
 
     /*
      * Pull files and reads their extensions and sets a filetype
@@ -135,8 +219,8 @@ public class fdsee {
 
             //Check that the arguments are valid
 
-            if(args.length == 0 || args.length == 1){
-                System.err.println("Error: Need at least two argument");
+            if(args.length == 0){
+                System.err.println("Error: Need at least one argument");
                 System.exit(1);
             }
             
@@ -146,41 +230,84 @@ public class fdsee {
             }
 
             String action = args[0];
-            String filename = args[1];
+            String filename = null;
+
+            if(args.length == 2)
+                if(args[1] != null)
+                    filename = args[1];
 
             htmlParser parse;
             
             switch(action){
                 
                 case "searchable":
-                    writer = new PrintWriter(SEARCH_OUTPUT, "UTF-8");
                     
+                    writer = new PrintWriter(SEARCH_OUTPUT, "UTF-8");
                     isSearchable(filename);
                     writer.close();
                     
                     break;
                
                 case "token":
-                   
-                    writer = new PrintWriter(SEARCH_OUTPUT, "UTF-8");
-                    isSearchable(filename);
-                    writer.close();
-                    
+                  
+                    if(filename != null){
+                        writer = new PrintWriter(SEARCH_OUTPUT, "UTF-8");
+                        isSearchable(filename);
+                        writer.close();
+                    } 
+
                     parse = new htmlParser(ParseTokenType.TOKEN);
                     parse.closeWriter(); 
                     break;
                 
                 case "tokendebug":
 
-                    writer = new PrintWriter(SEARCH_OUTPUT, "UTF-8");
-                    isSearchable(filename);
-                    writer.close();
-                    
+                    if(filename != null){
+                        writer = new PrintWriter(SEARCH_OUTPUT, "UTF-8");
+                        isSearchable(filename);
+                        writer.close();
+                    }
+
                     parse = new htmlParser(ParseTokenType.TOKENDEBUG);
                     parse.closeWriter(); 
                     break;
-               
+              
+                case "stopword":
+                    
+                    if(filename != null){
+                  
+                        writer = new PrintWriter(SEARCH_OUTPUT, "UTF-8");
+                        isSearchable(filename);
+                        writer.close();
+                    
+                        parse = new htmlParser(ParseTokenType.TOKEN);
+                        parse.closeWriter(); 
+                    
+                        parse = new htmlParser(ParseTokenType.TOKENDEBUG);
+                        parse.closeWriter(); 
+
+                    }
+                    
+                    mergeAndStopWords(new File(TOKEN_OUTPUT), new File(TOKENDEBUG_OUTPUT));
+                    break; 
                 case "stem":
+                    
+                    if(filename != null){
+                  
+                        writer = new PrintWriter(SEARCH_OUTPUT, "UTF-8");
+                        isSearchable(filename);
+                        writer.close();
+                    
+                        parse = new htmlParser(ParseTokenType.TOKEN);
+                        parse.closeWriter(); 
+                    
+                        parse = new htmlParser(ParseTokenType.TOKENDEBUG);
+                        parse.closeWriter(); 
+
+                        mergeAndStopWords(new File(TOKEN_OUTPUT), new File(TOKENDEBUG_OUTPUT));
+                    }
+                    
+                    mergeStem(new File(STOPWORD_OUTPUT));
                     break;
 
                 default:
